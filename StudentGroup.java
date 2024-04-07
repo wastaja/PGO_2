80f86cb5967910b7451c4ce70d41e99f6140efd4
Taja Wasilewicz <116358644+wastaja@users.noreply.github.com>
2024-04-07 15:39:00 +0200

    Update StudentGroup.java

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
 public class StudentGroup {
 
     String nazwa;
+    ArrayList<Student> students; // Lista studentów w grupie
 
+    public StudentGroup(String nazwa) {
+        this.nazwa = nazwa;
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        if (students.size() >= 15) {
+            throw new IllegalStateException("Grupa studencka jest już pełna.");
+        }
+
+        if (students.contains(student)) {
+            throw new IllegalStateException("Ten student jest już w tej grupie.");
+        }
+
+        students.add(student);
+    }
 }
