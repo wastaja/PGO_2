4f090edc2259aa9d7bf7a4a46601cd08ceea5bbb
Taja Wasilewicz <116358644+wastaja@users.noreply.github.com>
2024-04-07 15:38:38 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -5,33 +5,35 @@ public class Student {
     int indexNumber;
     String email;
     String address;
-    static int[] grades;
+    int[] grades;
+    int gradeCount;
 
-
-    public static void AverageGrade() {
-        int len = grades.length;
-
-
-        if (len >= 21) {
-            throw new IllegalArgumentException("Student może mieć maksymalnie 20 ocen.");
-
-        } else {
-            int sum = 0;
-            for (int number : grades) {
-                sum += number;
-            }
-            double avg = (double) sum / grades.length;
-            int output = (int) Math.round(avg);
-            System.out.println(output);
-            }
+    public Student(String fname, String lname, int indexNumber, String email, String address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.indexNumber = indexNumber;
+        this.email = email;
+        this.address = address;
+        this.grades = new int[20];
+        this.gradeCount = 0;
+    }
+    public double calculateAverageGrade() {
+        if (grades.length == 0) {
+            throw new IllegalArgumentException("Student nie ma żadnych ocen.");
         }
 
-    public static void AddToGroup() {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        double avg = (double) sum / grades.length;
+        return Math.round(avg * 2) / 2.0; // Zaokrąglanie do 0.5
+    }
+    public void addGrade(int grade) {
+        if (gradeCount == 20) {
+            throw new IllegalStateException("Student może mieć maksymalnie 20 ocen.");
+        }
 
+        grades[gradeCount++] = grade;
     }
 }
-
-
-
-
-
